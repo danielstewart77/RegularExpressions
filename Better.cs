cd30dd6bcd88fbcd5b61405f8a0af0452374cdb0
Daniel Stewart <daniel.stewart77@gmail.com>
2022-05-18 19:10:52 -0500

    Add project files.

---
+++ Better.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace RegularExpressionsExercises
+{
+	internal class Better
+	{
+        public bool IsValid(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionsExercises
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,})+)$";
+
+			string email = "";
+
+			do
+			{
+				Console.WriteLine("enter a valid email address:");
+				email = Console.ReadLine();
+			}
+
+			//while (!Regex.IsMatch(email, pattern));
+			while (!new Better().IsValid(email));
+
+			Console.WriteLine($"Thanks! {email} is valid");
+			Console.ReadKey();
+		}
+	}
+}
